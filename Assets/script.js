4236dd03991f9cb6ee66c5c15ebcc6cc6a532042
Omar Correa <omarcorrea378@yahoo.com>
2022-11-08 01:43:09 -0500

    added more js

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,15 @@
 const apiKey = "d5597b1e7a39fea80560c4ae65ba0cf7"
-var temp = document.querySelector("#temp")
-var cityName = document.querySelector("#cityName")
-var cityCoord = document.querySelector("#cityCoord")
-var dayOneTemp = document.createElement("h1")
-var temp = document.getElementById("dayone")
-var search = document.getElementById("search")
+var searchButton = document.getElementById("search")
 
 let forecastElem = document.getElementById("forecast-elem")
 console.log("forecastElem", forecastElem)
 
-search.addEventListener("click", function(){
-    cityLatlon()
+searchButton.addEventListener("click", function () {
+
+    let cityInput = document.getElementById("city-input")
+    console.log('this is the cityInput: ', cityInput, cityInput.value)
+
+    cityWeather(cityInput.value)
 })
 
 function cityWeather(cityName) {
@@ -22,19 +21,15 @@ function cityWeather(cityName) {
         })
         .then(function (data) {
             console.log(data)
-            cityLat = data[0].lat
-            cityLon = data[0].lon
+            var cityLat = data[0].lat
+            var cityLon = data[0].lon
             console.log("lat", cityLat)
             console.log("lon", cityLon)
             cityLatlon(cityLat, cityLon)
         })
 }
 
-cityLatlon('atlanta')
-
-function cityLatlon() {
-    console.log("lat", cityLat)
-    console.log("lon", cityLon)
+function cityLatlon(cityLat, cityLon) {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`)
         .then(function (response) {
             console.log(response)
@@ -42,31 +37,47 @@ function cityLatlon() {
         })
         .then(function (data) {
 
+        
+            let todaysWeatherElem = document.getElementById("todays-date-elem")
 
-            let index = 0
+            let cityNameElem = document.createElement("h1")
+            cityNameElem.textContent = data.city.name
+            todaysWeatherElem.appendChild(cityNameElem)
+            
+            let todaysDateElem = document.createElement("h1")
 
+            let splitDate = data.list[0].dt_txt.split(" ")[0].split("-")
+            let finalString = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`
+            todaysDateElem.textContent = finalString
+            todaysWeatherElem.appendChild(todaysDateElem)
+
+
+            
+            // create 5 day forecast elems and append them 
             for (var i = 0; i < 5; i++) {
                 let dateElem = document.createElement("h1")
                 let temperatureElem = document.createElement("h1")
                 let humElem = document.createElement("h1")
                 // create elem for icon here..prob a div not h1
 
-                temperatureElem.textContent = Math.round((data.list[index].main.temp - 273) * 1.8) + 32
-                dateElem.textContent = data.list[index].dt_txt
-                humElem.textContent = data.list[index].main.humidity + "%"
+                temperatureElem.textContent = Math.round((data.list[i*8].main.temp - 273) * 1.8) + 32
+
+                let splitDate = data.list[i*8].dt_txt.split(" ")[0].split("-")
+                let finalString = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`
+
+                dateElem.textContent = finalString
+                humElem.textContent = data.list[i*8].main.humidity + "%"
                 // let icon = ?
 
                 forecastElem.appendChild(dateElem)
                 forecastElem.appendChild(temperatureElem)
                 forecastElem.appendChild(humElem)
-
-                index = + 8
-
             }
-            
+
         })
-    }
-        
+}
+
+
 
 // function get5DaysForecast(apiResponse) {
 //     return apiResponse.list.reduce(function (days, current) {
