004b02dea51466dd2ed29f505953894ceef18d26
Omar Correa <omarcorrea378@yahoo.com>
2022-11-08 17:29:00 -0500

    more code added

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,11 @@
 const apiKey = "d5597b1e7a39fea80560c4ae65ba0cf7"
 var searchButton = document.getElementById("search")
+let forecastLabel = document.getElementById("forecast-label")
 
 let forecastElem = document.getElementById("forecast-elem")
 console.log("forecastElem", forecastElem)
 
 searchButton.addEventListener("click", function () {
-
     let cityInput = document.getElementById("city-input")
     console.log('this is the cityInput: ', cityInput, cityInput.value)
 
@@ -45,34 +45,53 @@ function cityLatlon(cityLat, cityLon) {
             todaysWeatherElem.appendChild(cityNameElem)
             
             let todaysDateElem = document.createElement("h1")
-
             let splitDate = data.list[0].dt_txt.split(" ")[0].split("-")
             let finalString = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`
             todaysDateElem.textContent = finalString
             todaysWeatherElem.appendChild(todaysDateElem)
 
+            let todaysTemp = document.createElement("h1")
+            todaysTemp.textContent = "Temperature: " + (Math.round((data.list[0].main.temp - 273) * 1.8) + 32) + "°F"
+            todaysWeatherElem.appendChild(todaysTemp)
+
+            let todaysHum = document.createElement("h1")
+            todaysHum.textContent =  "Humidity: " + (data.list[0].main.humidity) + "%"
+            todaysWeatherElem.appendChild(todaysHum)
+            
+
+
+
 
             
             // create 5 day forecast elems and append them 
             for (var i = 0; i < 5; i++) {
+                let wrapperDiv = document.createElement("div")
                 let dateElem = document.createElement("h1")
                 let temperatureElem = document.createElement("h1")
                 let humElem = document.createElement("h1")
                 // create elem for icon here..prob a div not h1
 
-                temperatureElem.textContent = Math.round((data.list[i*8].main.temp - 273) * 1.8) + 32
+                wrapperDiv.style.marginRight = "1rem"
+                wrapperDiv.style.margin = "1rem"
+                wrapperDiv.classList.add("card")
+
+                wrapperDiv.appendChild(dateElem)
+                wrapperDiv.appendChild(temperatureElem)
+                wrapperDiv.appendChild(humElem)
+
+                temperatureElem.textContent = "Temp: "+( Math.round((data.list[i*8].main.temp - 273) * 1.8) + 32 ) + "°F"
 
                 let splitDate = data.list[i*8].dt_txt.split(" ")[0].split("-")
                 let finalString = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`
+                // icon
 
                 dateElem.textContent = finalString
-                humElem.textContent = data.list[i*8].main.humidity + "%"
-                // let icon = ?
+                humElem.textContent = "Humidity: " + (data.list[i*8].main.humidity) + "%"
 
-                forecastElem.appendChild(dateElem)
-                forecastElem.appendChild(temperatureElem)
-                forecastElem.appendChild(humElem)
+                forecastElem.appendChild(wrapperDiv)
             }
+
+            forecastLabel.style.display = "block"
 
         })
 }
